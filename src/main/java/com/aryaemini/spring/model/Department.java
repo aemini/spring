6230a96748b98110549002621a40dc54d1988714
Arya Emini <arya@aryaemini.com>
2016-03-05 16:13:16 +0200

    bu haliyle calisiyor. Yolu ayirip thymeleaf kastiralim

--- src/main/java/com/aryaemini/spring/model/Department.java
+++ src/main/java/com/aryaemini/spring/model/Department.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 //import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.Size;
+//import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "departments")
